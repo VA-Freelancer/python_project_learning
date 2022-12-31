fc1af843f44bbe1b5375cabe863dbf7a38b2f265
VA-Freelancer <vproger@vk.com>
2022-12-31 18:21:49 +0400

    next learning python

--- cars.py
+++ cars.py
@@ -1,15 +1,21 @@
-cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars = ['audi', 'bmw', 'toyota', 'subaru']
 # cars.sort() сортировка по порядку, изменяет искходный массив
 # print(cars)
 # cars.sort(reverse=True) обратный порядок сортировки
 # print(cars)
 
-print("Here is the original list:")
-print(cars)
-print("\nHere is the sorted list:")
-print(sorted(cars)) # не изменяет исходный массив
-print("Here is the original list again:")
-print(cars)
+# print("Here is the original list:")
+# print(cars)
+# print("\nHere is the sorted list:")
+# print(sorted(cars)) # не изменяет исходный массив
+# print("Here is the original list again:")
+# print(cars)
+#
+# cars.reverse()
+# print(cars)
 
-cars.reverse()
-print(cars)
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
---
+++ practic/swedish_table.py
@@ -0,0 +1,12 @@
+# 4.13
+swedish_dishes = ("Салат Оливье", "Суп Борщ", "Пюре с котлетой", "Заварная пироженка", "Кофе", "Чай")
+for dish in swedish_dishes:
+    print(dish)
+
+# swedish_dishes[0] = "Салат Зимний"  - ошибка
+print("\nРесторан изменил меню\n")
+
+swedish_dishes = ("Салат Зимний", "Суп Щи", "Пюре с рыбой", "Пироженка Безе", "Кокао", "Чай")
+
+for dish in swedish_dishes:
+    print(dish)
